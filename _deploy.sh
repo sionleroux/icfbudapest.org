d1d5abbb884490b3f641f0e8221480a2394e3dca
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 20:38:27 +0200

    Remove _site/ part from FTP target URL

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f | xargs -I{} curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{}
+find _site -type f | echo xargs -I{} curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/${{}##_site/}
