d3763e1345bda389e9e15f4eee63972ca6fb7bac
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 21:29:43 +0200

    Retry when upload takes too long

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -printf "%P\n" | xargs -I{} curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T _site/{} ftp://ftp.sinisterstuf.org/{}
+find _site -type f -printf "%P\n" | xargs -I{} curl --connect-timeout 5 --retry 3 -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T _site/{} ftp://ftp.sinisterstuf.org/{}
