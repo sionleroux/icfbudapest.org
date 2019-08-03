e6c16a06e28a0594a547dd11de688d476575eaf0
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 17:37:32 +0200

    Use xargs for quitting find on first error

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -not -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{} \; -quit
+find _site -type f | xargs -I{} curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{}
