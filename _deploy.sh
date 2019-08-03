fc724cc4a07822382ffe90d16fe454106a86d13e
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 21:48:24 +0200

    Move glynn setup to travis before_deploy stage

--- _deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-# find _site -type f -printf "%P\n" | xargs -I{} curl --connect-timeout 5 --retry 3 -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T _site/{} ftp://ftp.sinisterstuf.org/{}
-
-echo "ftp_username: $FTP_USERNAME" >> _config.yml
-echo "ftp_password: $FTP_PASSWORD" >> _config.yml
-gem install glynn && glynn
