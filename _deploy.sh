67d8d646b9c2490e44fa370b8d5ae1b5e4fa120d
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 20:48:46 +0200

    Drop the echo -_-

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -printf "%P\n" | echo xargs -I{} curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T _site/{} ftp://ftp.sinisterstuf.org/{}
+find _site -type f -printf "%P\n" | xargs -I{} curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T _site/{} ftp://ftp.sinisterstuf.org/{}
