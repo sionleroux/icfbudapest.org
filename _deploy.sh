97f1662b4914936599d090b7a53c7760e006daa9
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 17:22:38 +0200

    Fix auth in FTP upload

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -exec curl -u xxx:psw --ftp-create-dirs -T {} ftp://$FTP_USERNAME:$FTP_PASSWORD@icfbudapest.org:21/{} \;
+find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://icfbudapest.org/{} \;
