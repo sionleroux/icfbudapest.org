d4c78effbd75d65af2ffaa8de561f58ae1cccb77
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 17:25:34 +0200

    Quit find after first upload failure

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://icfbudapest.org/{} \;
+find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://icfbudapest.org/{} \; -o -quit
