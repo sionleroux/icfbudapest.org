6cb2b7c08c3a19de9ce9044699eb69b94c2000e0
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 17:27:59 +0200

    Fix FTP hostname

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://icfbudapest.org/{} \; -o -quit
+find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{} \; -o -quit
