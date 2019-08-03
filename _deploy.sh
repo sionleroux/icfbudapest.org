be7e291332e7b94bd82b92a64b986f5c663a9fc9
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-03 17:31:29 +0200

    Try using not for breaking out of find

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
 
-find _site -type f -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{} \; -o -quit
+find _site -type f -not -exec curl --ftp-ssl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.sinisterstuf.org/{} \; -quit
