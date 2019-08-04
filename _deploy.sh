e0484a4d38cc18e1f4a7bcaa545fb2123c603e35
Siôn Le Roux <sinisterstuf@gmail.com>
2019-08-04 23:48:11 +0200

    Try ncftp instead of glynn

--- _deploy.sh
+++ _deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -ev
 
-# Set up auth
-echo "ftp_username: $FTP_USERNAME" >> _config.yml
-echo "ftp_password: $FTP_PASSWORD" >> _config.yml
+# # Set up auth
+# echo "ftp_username: $FTP_USERNAME" >> _config.yml
+# echo "ftp_password: $FTP_PASSWORD" >> _config.yml
 
-# Real work
-gem install glynn && glynn
+# # Real work
+# gem install glynn && glynn
+
+ncftpput -u $FTP_USERNAME -p $FTP_PASSWORD -S .tmp -R / _site/
